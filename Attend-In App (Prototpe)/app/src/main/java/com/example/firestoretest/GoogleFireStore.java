338c2975b0d57b71469677a964edbe9641afbcd0
John Nguyen <72109768+John9670@users.noreply.github.com>
2021-03-10 21:53:24 -0500

    Version 1.1

--- Attend-In App (Prototpe)/app/src/main/java/com/example/firestoretest/GoogleFireStore.java
+++ Attend-In App (Prototpe)/app/src/main/java/com/example/firestoretest/GoogleFireStore.java
@@ -1,4 +1,10 @@
 package com.example.firestoretest;
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -8,7 +14,49 @@ import java.util.Map;
 public class GoogleFireStore {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    public void setUserAcc(String name,String id){
+    public interface OnGetDataListener {
+        void onComplete(boolean success);
+    }
+
+    public void isValid(final String name, String id, final OnGetDataListener callback) {
+        DocumentReference user = db.collection("Users").document(id);
+        user.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    if(task.getResult().getString("Name") == null){
+                        callback.onComplete(false);
+                    }
+                    else if(task.getResult().getString("Name").equals(name)){
+                        callback.onComplete(true);
+                    }
+                    else{
+                        callback.onComplete(false);
+                    }
+                }
+            }
+        });
+    }
+
+    public void doesExist(String id, final OnGetDataListener callback){
+        DocumentReference user = db.collection("Users").document(id);
+        user.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    if(task.getResult().getString("Name") == null){
+                        callback.onComplete(false);
+                    }
+                    else{
+                        callback.onComplete(true);
+                    }
+                }
+            }
+        });
+
+    }
+
+    public void setUserAcc(String name, String id){
         Map<String, String> note = new HashMap<>();
         note.put("Name", name);
         db.collection("Users").document(id).set(note);
--- Attend-In App (Prototpe)/app/src/main/java/com/example/firestoretest/LoginActivity.java
+++ Attend-In App (Prototpe)/app/src/main/java/com/example/firestoretest/LoginActivity.java
@@ -1,12 +1,20 @@
 package com.example.firestoretest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class LoginActivity extends AppCompatActivity {
     private GoogleFireStore g;
@@ -22,29 +30,61 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         //From the login activity I've grabbed the two edit text and labeled to the respective global variables
-        editTextName = findViewById(R.id.username);
-        editTextId = findViewById(R.id.id);
-        buttonLogin = findViewById(R.id.login);
-        buttonLogin = findViewById(R.id.signup);
+        editTextName = findViewById(R.id.usernameText);
+        editTextId = findViewById(R.id.idText);
+        buttonLogin = findViewById(R.id.loginButton);
+        buttonLogin = findViewById(R.id.signupButton);
         g = new GoogleFireStore();
+
     }
 
     public void loginButton(View view) {
         //TODO check if valid signin else ask them for the info again ...
+        String name = editTextName.getText().toString();
+        String id = editTextId.getText().toString();
 
+        if(name.equals("") || id.equals("")){
+            Toast.makeText(LoginActivity.this, "Input Valid Name and Id", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            g.isValid(name,id, new GoogleFireStore.OnGetDataListener() {
+                public void onComplete(boolean success){
+                    if(success == true){
+                        Toast.makeText(LoginActivity.this, "Logging", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(LoginActivity.this, "Input Valid Name and Id", Toast.LENGTH_SHORT).show();
+                    }
 
-        Intent intent = new Intent(getBaseContext(), MapsActivity.class);
-        startActivity(intent);
-
+                }
+            });
+        }
     }
 
     public void signupButton(View view) {
         //TODO check if there is already an account else ...
+        final String name = editTextName.getText().toString();
+        final String id = editTextId.getText().toString();
 
-        g.setUserAcc(editTextName.getText().toString(),editTextId.getText().toString());
-
-        Intent intent = new Intent(getBaseContext(), MainActivity.class);
-        startActivity(intent);
-
+        if(name.equals("") || id.equals("")){
+            Toast.makeText(LoginActivity.this, "Input Valid Name and Id", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            g.doesExist(id, new GoogleFireStore.OnGetDataListener() {
+                public void onComplete(boolean success){
+                    if(!success == true){
+                        Toast.makeText(LoginActivity.this, "Making Account", Toast.LENGTH_SHORT).show();
+                        g.setUserAcc(name, id);
+                        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                        startActivity(intent);
+                    }
+                    else{
+                        Toast.makeText(LoginActivity.this, "Input Valid Id", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
